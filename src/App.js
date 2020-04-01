02540d4d4ad0704c24b2764149b1dd5897c897e6
Mathew <mjstthomas0516@gmail.com>
2020-03-31 18:10:18 -0700

    added button listeners

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import List from './List'
 import './App.css';
+import STORE from './STORE'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state ={
+    lists: STORE.lists,
+    cards: STORE.allCards
+  }
+  //random card button function
+  newRandomCard = () => {
+    const id = Math.random().toString(36).substring(2, 4)
+      + Math.random().toString(36).substring(2, 4);
+    return {
+      id,
+      title: `Random Card ${id}`,
+      content: 'lorem ipsum',
+    }
+  }
+  handleClickRandom = event => {
+  const random = this.newRandomCard();
+    const randomId = random.id;
+    const newLists = this.state.lists.map((currentList) => {
+      if (event.target.id === currentList.id){
+        const newArray = currentList.cardIds.concat(random.id)
+        currentList.cardIds = newArray
+      }
+      return currentList
+    })
+    const newAllCards = this.state.cards;
+    newAllCards[randomId] = random;
+    this.setState({
+        lists: newLists,
+        cards: newAllCards
+    })
+  }
+  //handleCardDelete
+  handleClickDelete = event =>{
+    console.log('deleted')
+  }
+  render() {
+    return (
+      <main className='App'>
+        <header className='App-header'>
+          <h1>Trelloyes!</h1>
+        </header>
+        <div className='App-list'>
+          {this.state.lists.map(list => (
+            <List
+              key={list.id}
+              id = {list.id}
+              header={list.header}
+              cards={list.cardIds.map(id => this.state.cards[id])}
+              handleClick = {this.handleClickRandom}
+              handleDelete = {this.handleClickDelete}
+            />
+          ))}
+        </div>
+      </main>
+    );
+  }
 }
 
 export default App;
